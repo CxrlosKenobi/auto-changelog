06591ca47a03380246034aada1198620600b87c3
CxrlosKenobi <yolitopinto@gmail.com>
2024-06-29 03:08:09 -0400

    feat(octokit): :sparkles: init separation of concerns and enhanced commits data cleaning

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,37 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import * as core from "@actions/core";
-import * as github from "@actions/github";
 import * as http from "@actions/http-client";
+//
+import { getCommits } from "./components/octokit/index.js";
+import { getChangelogBlock } from "./components/text-generation/index.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * NOTE: There's no separation of concerns yet, nor modularized code.
+ * since I'm testing the whole workflow in one file before splitting it up.
+ */
 
 async function run(): Promise<void> {
   try {
-    // This should be a token with access to the repository scoped in as a secret.
+    // These should be a token/key with scoped access to the repository, as a secret variable.
     const ghToken = core.getInput("github_repo_token", { required: true });
-    const openaiKey = core.getInput("openai_api_key", { required: true });
+    const openAIKey = core.getInput("openai_api_key", { required: true });
     core.setSecret(ghToken);
-    core.setSecret(openaiKey);
+    core.setSecret(openAIKey);
 
-    const context = github.context;
-    const commits = await getCommits({ ghToken, context }); // Getting commits metadata
+    const commits = await getCommits({ ghToken }); // Getting commits metadata using Octokit APIs
     //
-    const changelog = await generateChangelog(commits); // Generating changelog using LLM APIs
+    const changelog = await getChangelogBlock({ apiKey: openAIKey, commits, __dirname }); // Generating changelog content using LLM APIs
+    core.info(`Changelog generation:\n${changelog}`);
+
     const version = determineVersion(commits); // Determining version based on semantic commits actions
 
   } catch (error: any) {
     core.setFailed(error.message);
   }
-}
-
-async function getCommits({ ghToken, context }: { ghToken: string; context: any }) {
-  if (context.payload.pull_request) {
-    const pr = context.payload.pull_request;
-    core.info(`PR number: ${pr.number}`);
-
-  } else {
-    core.info("No pull request found.");
-    return [];
-  }
-
-  const octokit = github.getOctokit(ghToken);
-  const { data: commits } = await octokit.rest.pulls.listCommits({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    pull_number: context.issue.number
-  });
-
-  core.info(`Commits: ${commits}`);
-  core.info(`Commits count: ${commits.length}`);
-}
-
-async function generateChangelog(commits: any) {
 }
 
 async function determineVersion(commits: any) {
